c6698775826f12574a1fa71595cb8bb893d731fe
varaprasad431 <147475432+varaprasad431@users.noreply.github.com>
2024-07-17 12:00:16 +0530

    Create jenkins.sh

---
+++ shellscripting/jenkins.sh
@@ -0,0 +1,23 @@
+# jenkins for amazon linux machine
+
+sudo yum update –y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade
+
+sudo dnf install java-17-amazon-corretto -y
+
+
+sudo yum install jenkins -y
+
+
+sudo systemctl enable jenkins
+ 
+sudo systemctl start jenkins
+
+sudo systemctl status Jenkins
+
+
